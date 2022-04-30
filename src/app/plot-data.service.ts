13fbd2a09684ae60edc0124ed9c3dfb19c2b4dd5
mrgne1 <mrgne1@gmail.com>
2022-04-29 19:51:24 -0700

    Got the plot-data service working with the HttpClient to query the newly created data on the newly created data server

--- src/app/plot-data.service.ts
+++ src/app/plot-data.service.ts
@@ -1,40 +1,44 @@
 import { Injectable } from '@angular/core';
-
-const lineTrace = {
-  type: 'scatter',
-  mode: 'lines',
-  x: [1, 2, 3, 4],
-  y: [2, 6, 3, 3],
-  line: {
-    color: 'red',
-  },
-};
-
-const barTrace = {
-  type: 'bar',
-  x: [1, 2, 3, 4],
-  y: [2, 5, 3, 3],
-};
-
-const layout = {
-  width: 320,
-  height: 240,
-  title: 'A Fancy Plot'
-};
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { concat, from, map, merge, Observable, OperatorFunction, zip } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlotDataService {
 
-  constructor() { }
+  constructor(
+    protected http: HttpClient,
+  ) { }
 
-  data(url: string, parameters: {[key: string]: string}): any {
-    console.log({url, parameters})
-    const graph = {
-       data: [lineTrace, barTrace],
-       layout: layout,
-    };
-    return JSON.parse(JSON.stringify(graph));
+  private maps: {[url: string]: OperatorFunction<any[], any>} = {
+    'http://localhost:3000/lineDemo': map((lineData: any[]) => {
+      return {
+        type: 'scatter',
+        mode: 'lines',
+        x: lineData.map((item) => item.x),
+        y: lineData.map((item) => item.y),
+        line: {
+          color: 'red',
+        },
+      }
+    }),
+    'http://localhost:3000/barDemo': map((barData: any[]) => {
+      return {
+        type: 'bar',
+        x: barData.map((item) => item.x),
+        y: barData.map((item) => item.y),
+      }
+    }),
+  }
+
+  data(urls: string[], parameters: {[key: string]: string|number|boolean}): Observable<any[]> {
+    let params = new HttpParams()
+    Object.entries(parameters).forEach(
+      (entry) => {
+        params = params.append(entry[0], entry[1]);
+      }
+    )
+    return zip(urls.map((url: string) => this.http.get<any>(url, {params}).pipe(this.maps[url])));
   }
 }
